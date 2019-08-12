29dbdfe2403ebd7f4cdaf2385caffa576aa45be9
Agnete Moos <agms@sonderborg.dk>
2019-08-12 13:34:03 +0200

    Works without the groupby

--- Resources/public/apps/toolsModule/sonderborg-calendar-editor.js
+++ Resources/public/apps/toolsModule/sonderborg-calendar-editor.js
@@ -6,6 +6,59 @@ angular.module('toolsModule').directive('sonderborgCalendarEditor', function(){
       slide:'=',
       close: '&'
     },
+    link: function (scope) {
+      function resetInputEvent() {
+        scope.newEvent = {
+          "title": null,
+          "place": null,
+          "from": null,
+          "to": null
+        };
+      }
+      resetInputEvent();
+
+      /**
+       * Add event to slide
+       */
+      scope.newEventItem = function newEventItem() {
+        console.log(scope.slide.options.eventitems);
+        // Add event data to slide array.
+        scope.slide.options.eventitems.push(angular.copy(scope.newEvent));
+
+        resetInputEvent();
+      };
+
+      /**
+       * Remove event from slide.
+       */
+      scope.removeEventItem = function removeEventItem(event) {
+        scope.slide.options.eventitems.splice(scope.slide.options.eventitems.indexOf(event), 1);
+      };
+
+      /**
+       * Sort events for slide.
+       */
+      scope.sortEvents = function sortEvents() {
+        if (scope.slide.options.eventitems.length > 0) {
+          // Sort the events by from date.
+          scope.slide.options.eventitems = $filter('orderBy')(scope.slide.options.eventitems, "from")
+        }
+      };
+
+      /**
+       * Is outdated for events on slide
+       */
+      scope.eventIsOutdated = function setOutdated(event) {
+        var to = event.to;
+        var from = event.from;
+        var now = Date.now() / 1000;
+
+        return (to && now > to) || (!to && now > from);
+      };
+
+      // Run sorting of events.
+      scope.sortEvents();
+    },
     templateUrl: '/bundles/bibsdbsonderborgcalendar/apps/toolsModule/sonderborg-calendar-editor.html'
   };
 });
